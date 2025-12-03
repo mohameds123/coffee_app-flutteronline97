85de99f8f8cdb776cd78ce0ec4f9107840620eee
AMIT <za.mohamedsaleh011213@gmail.com>
2025-12-03 22:05:56 +0200

    --

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -7,10 +8,41 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
           // layer1
-          Image.asset("assets/images/login_background.png"),
+          Image.asset("assets/images/login_background.png",width: double.infinity,fit: BoxFit.cover,),
+          //layer2
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 42.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+
+              children: [
+
+                Text("Coffee so good, your taste buds will love it.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 34,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600
+                ),
+                ),
+
+                Text("The best grain, the finest roast, the powerful flavor.",
+                  textAlign: TextAlign.center,
+
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w400
+                  ),
+                )
+              ],
+            ),
+          ),
+
         ],
       ),
     );
